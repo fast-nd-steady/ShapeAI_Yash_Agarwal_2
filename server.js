27a8addc1dec787bfddc07363d8d6699937a59f3
fast-nd-steady <68840882+fast-nd-steady@users.noreply.github.com>
2021-05-17 09:38:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const { response } = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res){ 
+    var n1=Number(req.body.num1);
+    var n2=Number(req.body.num2);
+    res.send("The result is " + String(n1+n2));
+});
+app.listen(3001,function(){
+    console.log("hello, the server has started on port 3001");
+});
